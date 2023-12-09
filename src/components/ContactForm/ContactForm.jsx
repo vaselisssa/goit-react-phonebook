2f9446e52745fdbc15b5fdaf358d329d91e664f1
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-12-09 19:06:41 +0200

    add autoFocus to ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -37,7 +37,7 @@ const ContactForm = ({ onAddContact }) => {
       <Form autoComplete="off">
         <Label>
           Name:
-          <Input type="text" name="name" />
+          <Input type="text" name="name" autoFocus />
           <FormError name="name" />
         </Label>
         <Label>
